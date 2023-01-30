0260e7fc46536402c76b22251e8c69fbe5cf06e9
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-01-30 19:49:25 +0300

    Portfolio menu options

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const menuButton = document.querySelector('#btn-menu');
-const closeModalIcon = document.querySelector('#modal-close-icon img')
-const mobileMenuModal = document.querySelector('#mobile-menu-modal')
-const menuLinks = document.querySelectorAll('#menu-link li')
-
-
-
+const closeModalIcon = document.querySelector('#modal-close-icon img');
+const mobileMenuModal = document.querySelector('#mobile-menu-modal');
+const menuLinks = document.querySelectorAll('#menu-link li');
 
 const toggleMenuDisplay = () => {
   mobileMenuModal.classList.toggle('toogleModalDisplay');
-}
+};
 
-menuButton.addEventListener('click',toggleMenuDisplay);
+menuButton.addEventListener('click', toggleMenuDisplay);
 
-closeModalIcon.addEventListener('click',toggleMenuDisplay);
+closeModalIcon.addEventListener('click', toggleMenuDisplay);
 
-menuLinks.forEach((link) =>{
-  link.addEventListener('click',toggleMenuDisplay)
-})
+menuLinks.forEach((link) => {
+  link.addEventListener('click', toggleMenuDisplay);
+});
